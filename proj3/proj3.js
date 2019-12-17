bbb16ca0492f32ffabdde221d9656538ae37b790
umbum <umbum7601@gmail.com>
2019-12-17 22:55:32 +0900

    [#2] (0,0,0)에 point light를 설정(태양 빛)
    upper, lower viewport 모두 동일한 하나의 (0,0,0) point light의 영향을 받음.
    lower viewport는 빛을 정면에서 받는 것을 보여주기 위해 0,0,0 지점에서 0,0,-1을 바라보는 방향으로 설정

--- proj3/proj3.js
+++ proj3/proj3.js
@@ -4,8 +4,7 @@
  */
 "use strict";
 
-function main()
-{
+function main() {
 	let canvas = document.getElementById('webgl');
 	let gl = canvas.getContext("webgl2");
 
@@ -16,15 +15,17 @@ function main()
     V.setLookAt(6, 4, 6, 0, 0, 0, 0, 1, 0);
 
 	let P = new Matrix4();
-    P.setPerspective(60, (canvas.width / canvas.height) * 2, 1, 100); 
+	P.setPerspective(60, (canvas.width / canvas.height) * 2, 1, 100); 
+	
+	const V_lower = new Matrix4();
+	V_lower.setLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
     const P_lower = new Matrix4();
     P_lower.setPerspective(60, (canvas.width / canvas.height), 1, 100);
     
 	let list_shaders = [];
 
 	// initializes shaders (reflection models)
-	for(let model of ["Blinn-Gouraud", "Phong-Gouraud", "Blinn-Phong", "Phong-Phong"])
-	{
+	for(let model of ["Blinn-Gouraud", "Phong-Gouraud", "Blinn-Phong", "Phong-Phong"]) {
 		list_shaders[model] = new Shader(gl, 
 			document.getElementById("vert-" + model).text,
 			document.getElementById("frag-" + model).text,
@@ -33,8 +34,7 @@ function main()
 
 	// initializes the material combobox
 	let combo_mat = document.getElementById("materials");
-	for(let matname in __js_materials)
-	{
+	for(let matname in __js_materials) {
 		let opt = document.createElement("option");
 		opt.value = matname;
 		opt.text = matname;
@@ -42,65 +42,38 @@ function main()
 	}
 	combo_mat.selectedIndex = 10;
 
-	// initializes two lights
-	let list_lights = 
-	[
-		new Light
-		(
+	// initializes light
+	const list_lights = [
+		new Light(  // sun light
 			gl,
-			[1.5, 1.5, 0, 1.0],		// position
-			[0.1, 0.1, 0.1, 1.0],	// ambient
-			[1.0, 1.0, 1.0, 1.0],	// diffusive
-			[1.0, 1.0, 1.0, 1.0],	// specular
+			[  0,   0,   0, 1.0],    // position
+			[0.1, 0.1, 0.1, 1.0],    // ambient
+			[1.0, 1.0, 1.0, 1.0],    // diffusive
+			[1.0, 1.0, 1.0, 1.0],    // specular
 			false
-		),
-		new Light
-		(
-			gl,
-			[, 1.5, 1.5, 1.0],
-			[0.1, 0.1, 0.1, 1.0],
-			[1.0, 1.0, 1.0, 1.0],
-			[1.0, 1.0, 1.0, 1.0],
-			false
-		)
-	];
+		)];
 
 	// initializes the meshes
-	let list_meshes = [];
     let sun = create_mesh_sphere(gl, 100);
     sun.M.setScale(0.7, 0.7, 0.7);
     let earth = create_mesh_sphere(gl, 100);
     let moon = create_mesh_sphere(gl, 100);
     
-
 	let axes = new Axes(gl);
 
-	let t_last = Date.now();
-
-	const ANGLE_STEP_LIGHT = 30.0;
-	const ANGLE_STEP_MESH = 30.0;
-	let tick = function()
-	{
+	let tick = function() {
 		let now = Date.now();
-		let elapsed = now - t_last;
-        t_last = now;
         
-		list_lights[0].M.rotate(( (ANGLE_STEP_LIGHT * elapsed) / 1000.0) % 360.0, 0, 1, 0);
-		if(document.getElementById("mesh-rotating").checked)
-			list_meshes[document.getElementById("objects").value].M.rotate(
-				-((ANGLE_STEP_MESH * elapsed) / 1000.0) % 360.0, 0, 1, 0);
-
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         
         gl.viewport(0, canvas.height/2, canvas.width, canvas.height/2);
 
 		axes.render(gl, V, P);
         
-		for(let i in list_lights)
-		{
+		for(let i in list_lights) {
 			// for light state and render it
-			list_lights[i].turn_on(document.getElementById('light' + i).checked);
-			list_lights[i].set_type(document.getElementById('light' + i + "-type").value == "positional");
+			list_lights[i].turn_on(true);
+			list_lights[i].set_type(true);
 			list_lights[i].render(gl, V, P);
         }
         
@@ -137,19 +110,19 @@ function main()
 
         // lower-left viewport
         gl.viewport(0, 0, canvas.width/2, canvas.height/2);
-        earth.M.setScale(2, 2, 2);
+		earth.M.setTranslate(0, 0, -4);
         earth.render(gl, 
             list_shaders[document.getElementById("shading-models").value],
             list_lights,
-            __js_materials["gold"], V, P_lower);
+            __js_materials["gold"], V_lower, P_lower);
         
         // lower-right viewport
         gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2)
-        moon.M.setScale(2, 2 ,2);
+		moon.M.setTranslate(0, 0, -4);
         moon.render(gl, 
             list_shaders[document.getElementById("shading-models").value],
             list_lights,
-            __js_materials["emerald"], V, P_lower);
+            __js_materials["emerald"], V_lower, P_lower);
 
 		requestAnimationFrame(tick, canvas); // Request that the browser calls tick
 	};
