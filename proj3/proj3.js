3ad8bd36c5efc1c4feb7287f13ec229c8a7951c1
umbum <umbum7601@gmail.com>
2019-12-19 14:44:18 +0900

    [#2] Texture mapping 방향을 제대로 설정하기 위해 updirection을 z축으로 설정
    관련 자전축, 공전축 등 변경

--- proj3/proj3.js
+++ proj3/proj3.js
@@ -47,13 +47,13 @@ function main() {
     gl.clearColor(0.2, 0.2, 0.2, 1.0);
     
     let V = new Matrix4();
-    V.setLookAt(6, 4, 6, 0, 0, 0, 0, 1, 0);
+    V.setLookAt(6, 6, 4, 0, 0, 0, 0, 0, 1);
 
     let P = new Matrix4();
     P.setPerspective(60, (canvas.width / canvas.height) * 2, 1, 100); 
     
     const V_lower = new Matrix4();
-    V_lower.setLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+    V_lower.setLookAt(0, 0, 0, 0, -1, 0, 0, 0, 1);
     const P_lower = new Matrix4();
     P_lower.setPerspective(60, (canvas.width / canvas.height), 1, 100);
     
@@ -143,15 +143,15 @@ function main() {
          * rotate 후 translate : 공전
          * translate 후 rotate : 자전
          */
-        earth.M.rotate(earth_stat.revolving_angle, 0, 1, 0);
+        earth.M.rotate(earth_stat.revolving_angle, 0, 0, 1);
         earth.M.translate(6, 0, 0);
         
         // moon에서 earth를 기반으로 공전하기 위해 저장
         let earth_base = new Matrix4(earth.M);
 
         earth.M.rotate(earth_stat.rotating_angle,
-                     Math.sin(degree_to_rad(23.5)), 
-                     Math.cos(degree_to_rad(23.5)), 0);
+                     -Math.sin(degree_to_rad(23.5)), 
+                     0, Math.cos(degree_to_rad(23.5)));
         earth.render(gl, 
             list_shaders[document.getElementById("shading-models").value],
             list_lights,
@@ -159,9 +159,9 @@ function main() {
         
         moon.M = earth_base;
         moon.M.scale(0.6, 0.6, 0.6);
-        moon.M.rotate(moon_stat.revolving_angle, 0, 1, 0);
+        moon.M.rotate(moon_stat.revolving_angle, 0, 0, 1);
         moon.M.translate(3.5, 0, 0);
-        moon.M.rotate(moon_stat.rotating_angle, 0, 1, 0);
+        moon.M.rotate(moon_stat.rotating_angle, 0, 0, 1);
         moon.render(gl, 
             list_shaders[document.getElementById("shading-models").value],
             list_lights,
@@ -169,10 +169,10 @@ function main() {
 
         // lower-left viewport
         gl.viewport(0, 0, canvas.width/2, canvas.height/2);
-        earth.M.setTranslate(0, 0, -4);
+        earth.M.setTranslate(0, -4, 0);
         earth.M.rotate(earth_stat.rotating_angle, 
-                    Math.sin(degree_to_rad(23.5)), 
-                    Math.cos(degree_to_rad(23.5)), 0);
+                    -Math.sin(degree_to_rad(23.5)), 
+                    0, Math.cos(degree_to_rad(23.5)));
         earth.render(gl, 
             list_shaders[document.getElementById("shading-models").value],
             list_lights,
@@ -180,8 +180,8 @@ function main() {
         
         // lower-right viewport
         gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2)
-        moon.M.setTranslate(0, 0, -4);
-        moon.M.rotate(moon_stat.rotating_angle, 0, 1, 0);
+        moon.M.setTranslate(0, -4, 0);
+        moon.M.rotate(moon_stat.rotating_angle, 0, 0, 1);
         moon.render(gl, 
             list_shaders[document.getElementById("shading-models").value],
             list_lights,
